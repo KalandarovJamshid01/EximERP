dfe33fc06f6b0bf8bb70cbf26abae37c3912fcaa
Jamshid <kalandarovjamshid01@gmail.com>
2023-01-12 23:20:43 +0500

    file download is ready

--- controller/document.js
+++ controller/document.js
@@ -53,40 +53,68 @@ let upload = multer({
 
 const addFile = async (req, res) => {
   //Storage file
-  upload(req, res, async (err) => {
-    //Validate request
+  try {
+    upload(req, res, async (err) => {
+      //Validate request
 
-    if (!req.files) {
-      return res.json({ error: "All fileds are required" });
-    }
+      if (!req.files) {
+        return res.json({ error: "All fileds are required" });
+      }
 
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
 
-    let objElements = {};
-    for (file in req.files) {
-      objElements[file] = req.files[file][0].filename;
-    }
+      let objElements = {};
+      for (file in req.files) {
+        objElements[file] = req.files[file][0].filename;
+      }
 
-    objElements["comment"] = req.body.comment;
-    objElements["tip_procedure"] = req.body.tip_procedure;
-    objElements["tip_rejim"] = req.body.tip_rejim;
-    objElements["comment"] = req.body.comment;
-    objElements["TNVED"] = req.body.TNVED;
-    objElements["sostavleniye_docs"] = req.body.sostavleniye_docs;
-    objElements["user._id"] = req.user._id;
-    const fileDB = new Document(objElements);
-    console.log(fileDB);
-    // Store in Database
+      objElements["tip_procedure"] = req.body.tip_procedure;
+      objElements["tip_rejim"] = req.body.tip_rejim;
+      objElements["comment"] = req.body.comment;
+      objElements["TNVED"] = req.body.TNVED;
+      objElements["sostavleniye_docs"] = req.body.sostavleniye_docs;
+      objElements["user._id"] = req.user._id;
+      const fileDB = new Document(objElements);
+      console.log(fileDB);
+      // Store in Database
 
-    const response = await fileDB.save();
+      const response = await fileDB.save();
 
-    return res.json({
-      message: "Success",
-      response,
+      return res.json({
+        message: "Success",
+        response,
+      });
     });
-  });
+  } catch (err) {
+    return res.json(err);
+  }
 };
 
-module.exports = { addFile };
+const get_all_doc = async (req, res) => {
+  try {
+    const documents = await Document.find();
+
+    return res.status(200).json(documents);
+  } catch (error) {
+    return res.json(error);
+  }
+};
+get_one_document = async (req, res, next) => {
+  try {
+    const document = await Document.findById(req.params.id);
+
+    return res.status(200).json(document);
+  } catch (error) {
+    return res.json(error);
+  }
+};
+const downloadFile = async (req, res) => {
+  console.log("hello");
+  const file = await Document.findById(req.params.id);
+
+  const filePath = `files/${req.params.doc_file}`;
+  res.download(filePath);
+};
+module.exports = { addFile, downloadFile };
--- route/document.js
+++ route/document.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const document = require("./../controller/document");
 const verify = require("./../verify.js");
 router.route("/").post(verify, document.addFile);
+router.route("/:doc_file").get(document.downloadFile);
 // router
 //   .route("/:id")
 //   .get(document.getOne)
